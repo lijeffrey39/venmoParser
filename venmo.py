77f0efb08e55747a638228706144c397a532e71a
Debanik Purkayastha <debanikpurkayastha@Debaniks-Air.fios-router.home>
2019-08-21 00:32:15 -0400

    added jupyter notebook for EDA

--- venmo.py
+++ venmo.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium import webdriver
 import time
+import pandas as pd
+import os
 
 f = open('session_info.txt', 'r')
 command_executor = f.readline().rstrip()
@@ -9,26 +11,44 @@ session_id = f.readline().rstrip()
 driver = webdriver.Remote(command_executor = command_executor)
 driver.session_id = session_id
 
-while (driver.find_element_by_class_name('moreButton').text == "More"):
-	more = driver.find_element_by_class_name('moreButton')
-	more.click()
-	time.sleep(2)
+# while (driver.find_element_by_class_name('moreButton').text == "More"):
+# 	try:
+# 		more = driver.find_element_by_class_name('moreButton')
+# 		more.click()
+# 		# time.sleep(2)
+# 	except:
+# 		print("still going")
 
-allGreen = driver.find_elements_by_class_name('p_ten_t')
-profit = 0
 
-for g in allGreen:
-	third = g.find_elements_by_tag_name('td')
-	if (len(third) == 3):
-		spanTd = third[2].find_element_by_tag_name('span').text
-		print(spanTd)
-		num = float(spanTd[2:])
-		if (spanTd[0] == '-'):
-			profit = profit - num
+transactions = driver.find_elements_by_class_name('p_ten_t')[::-1]
+
+results = []
+current_balance = 0
+
+for transaction in transactions:
+	elements = transaction.find_elements_by_tag_name('td')
+	if (len(elements) == 3):
+		info = elements[1].find_element_by_class_name('width350').text
+		money_text = elements[2].find_element_by_tag_name('span').text
+		amount = float(money_text[2:])
+		sign = money_text[0]
+		charge = info.split('\n')
+		to_from = charge[0]
+		user = ''
+		if (sign == '+'):
+			current_balance += amount
 		else:
-			profit = profit + num
+			current_balance -= amount
 
-print(profit)
+		results.append([to_from, sign, amount, current_balance])
+
+
+# print(resul)
+
+df = pd.DataFrame(results) 
+current_path = os.getcwd()
+df.to_csv(current_path + '.csv')
+print("done")
 
 foo = input()
 
